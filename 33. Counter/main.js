3e8caa88febc621468d90f96d4fed8b1b2604a04
unknown <sondeptroi@gmail.com>
2025-03-28 21:52:30 +0700

    33. counter

---
+++ 33. Counter/main.js
@@ -0,0 +1,18 @@
+const incrementBtn = document.querySelector(".increment-btn");
+const saveBtn = document.querySelector(".save-btn");
+const countEl = document.querySelector("#count");
+const saveEl = document.querySelector("#save");
+
+let counts = 0;
+
+incrementBtn.addEventListener("click", () => {
+	counts += 1;
+	countEl.textContent = counts;
+});
+
+saveBtn.addEventListener("click", () => {
+	let countStr = counts + " , ";
+	saveEl.textContent += countStr;
+	countEl.textContent = 0;
+	counts = 0;
+});
