1e99e0f8a336f6e4c27bee2532f74076cc83dc0c
unknown <sondeptroi@gmail.com>
2025-03-29 20:03:34 +0700

    38. keycode

---
+++ 38. keycode/main.js
@@ -0,0 +1,25 @@
+const insert = document.querySelector("#insert");
+
+window.addEventListener("keydown", (event) => {
+	// console.log(event.key);
+	// console.log(event.code);
+	// console.log(event.keyCode);
+
+	insert.innerHTML = `
+     <div class="key">
+        ${event.key === "" ? "space" : event.key}
+        <small>event.key</small>
+    </div>;
+    
+    <div class="key">
+    ${event.keyCode}
+    <small>event.keyCode</small>
+    </div>;
+    
+    <div class="key">
+    ${event.code} 
+    <small>event.code</small>
+    </div>;
+    
+    `;
+});
