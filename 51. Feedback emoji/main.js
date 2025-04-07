5ad0a6a7783a0895d478c4438ed84ae2224b8022
unknown <sondeptroi@gmail.com>
2025-04-07 13:00:54 +0700

    faster faster

--- 51. Feedback emoji/main.js
+++ 51. Feedback emoji/main.js
@@ -1,11 +1,29 @@
-const emotions = document.querySelectorAll(".emotion");
-const btn = document.querySelector("#send");
-const container = document.querySelector("#container");
-const thanks = document.querySelector("#thanks");
+const ratings = document.querySelectorAll(".rating");
+const ratingsContainer = document.querySelector(".ratings-container");
+const sendBtn = document.querySelector("#send");
+const panel = document.querySelector("#panel");
 
+let selectedRating = "Satisfied";
 
-
-btn.addEventListener("click", () => {
-	thanks.classList.add("active");
-	container.style.display = "none";
+ratingsContainer.addEventListener("click", (e) => {
+	if (e.target.parentNode.classList.contains("rating")) {
+		removeActives();
+		e.target.parentNode.classList.add("active");
+		selectedRating = e.target.nextElementSibling.innerHTML;
+	}
 });
+
+sendBtn.addEventListener("click", () => {
+	panel.innerHTML = `
+        <p class="heart">💖</p>
+        <strong>Thank You! </strong>
+        <br>
+        <strong>Feedback: ${selectedRating} </strong>
+    `;
+});
+
+function removeActives() {
+	for (let i = 0; i < ratings.length; i++) {
+		ratings[i].classList.remove("active");
+	}
+}
--- 52. eyes follow/mainn.js
+++ 52. eyes follow/mainn.js
@@ -3,12 +3,17 @@ document.querySelector("body").addEventListener("mousemove", eyeball);
 function eyeball() {
 	let eyes = document.querySelectorAll(".eye");
 	eyes.forEach((eye) => {
+		// Calculate Eye Center:
+
 		let x = eye.getBoundingClientRect().left + eye.clientWidth / 2;
 		let y = eye.getBoundingClientRect().top + eye.clientWidth / 2;
 
+		// caculate angle
 		let radian = Math.atan2(event.pageX - x, event.pageY - y);
 
 		let rotate = radian * (180 / Math.PI) * -1 + 270;
+
+		// Rotate the Eye:
 
 		eye.style.transform = `rotate(${rotate}deg)`;
 	});
