23e4cbb910af26f0d0b3c20aa2488cb2e54c8770
unknown <sondeptroi@gmail.com>
2025-04-07 15:05:50 +0700

    68. boxshadown

---
+++ 68. boxshadow generater/main.js
@@ -0,0 +1,38 @@
+const sliders = document.querySelectorAll('input[type="range"]');
+const colors = document.querySelectorAll('input[type="color"]');
+let output = document.getElementById("css-code");
+let btnCopy = document.getElementById("copy");
+
+sliders.forEach((slider) => {
+	slider.addEventListener("input", createBox);
+});
+
+colors.forEach((color) => {
+	color.addEventListener("change", createBox);
+});
+
+btnCopy.addEventListener("click", () => {
+	output.select();
+	document.execCommand("copy");
+	alert("code Copied");
+});
+
+function createBox() {
+	let X = sliders[0].value;
+	let Y = sliders[1].value;
+	let blur = sliders[2].value;
+	let spread = sliders[3].value;
+
+	let shadowColor = colors[0].value;
+
+	let boxShadow = `${X}px ${Y}px ${blur}px  ${spread}px `;
+
+	// change box
+	document.getElementById(
+		"box"
+	).style.cssText = `box-shadow: ${boxShadow} ${shadowColor}  `;
+
+	output.value = `box-shadow:${boxShadow} `;
+}
+
+createBox();
